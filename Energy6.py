3b85e97d341a92b7f18a3e1f724d6c6e0a20d303
L <60981745+pankourgiou@users.noreply.github.com>
2025-03-06 11:10:00 +0200

    Create Energy6.py

---
+++ Energy6.py
@@ -0,0 +1,9 @@
+#Setting 123 = 1 we will have:
+print("E = 123*mcmod2")
+#Setting Open_society = 1 we have:
+print("E = Open_society*mcmod2")
+#Set Analysis = 1 then Analysis = E/mcmod2
+#Setting 15602 = 1 we have:
+print("E = 15602*mcmod2")
+#Setting Alienmaschine = 1 we have:
+print("E = Alienmaschine*mcmod2")
